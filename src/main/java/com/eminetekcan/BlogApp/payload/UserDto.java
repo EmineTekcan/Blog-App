b4c84114efd2bca05b60553132c4b536d82eea85
Emine Tekcan <eminetek68can@gmail.com>
2023-03-08 14:37:38 +0300

    Validation added

--- src/main/java/com/eminetekcan/BlogApp/payload/UserDto.java
+++ src/main/java/com/eminetekcan/BlogApp/payload/UserDto.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.*;
+
 @Data
 @Builder
 @AllArgsConstructor
@@ -12,8 +14,18 @@ import lombok.NoArgsConstructor;
 public class UserDto {
 
     private int id;
+
+    @NotEmpty
+    @Size(min = 4,message = "Name must be min of 4 characters !!")
     private String name;
+
+    @NotEmpty
+    @Size(min = 5, max = 15, message = "Password must be min of 5 chars and max of 15 chars")
     private String password;
+
+    @Email(message = "Your email address not valid !!")
     private String email;
+
+    @NotNull
     private String about;
 }
