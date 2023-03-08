3d674b9e745019a0e1ce8ce369d8679bcd8b6b2e
Emine Tekcan <eminetek68can@gmail.com>
2023-03-08 14:36:15 +0300

    Method argument not valid exception handler added

--- src/main/java/com/eminetekcan/BlogApp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/eminetekcan/BlogApp/exception/GlobalExceptionHandler.java
@@ -3,8 +3,13 @@ package com.eminetekcan.BlogApp.exception;
 import com.eminetekcan.BlogApp.payload.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -17,5 +22,16 @@ public class GlobalExceptionHandler {
         apiResponse.setMessage(message);
         apiResponse.setSuccess(false);
         return new ResponseEntity<>(apiResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException exception){
+        Map<String, String> err=new HashMap<>();
+        exception.getBindingResult().getAllErrors().forEach(objectError -> {
+            String fieldName=((FieldError) objectError).getField();
+            String message=objectError.getDefaultMessage();
+            err.put(fieldName,message);
+        });
+        return new ResponseEntity<>(err,HttpStatus.BAD_REQUEST);
     }
 }
