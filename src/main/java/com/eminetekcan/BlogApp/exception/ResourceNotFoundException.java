20113406f52ea9f30d9b7a841a93dcd60d9e9a81
Emine Tekcan <eminetek68can@gmail.com>
2023-03-08 12:58:51 +0300

    resource not found exception

--- src/main/java/com/eminetekcan/BlogApp/exception/ResourceNotFoundException.java
+++ src/main/java/com/eminetekcan/BlogApp/exception/ResourceNotFoundException.java
@@ -2,7 +2,13 @@ package com.eminetekcan.BlogApp.exception;
 
 public class ResourceNotFoundException extends RuntimeException{
 
+    private String resourceName;
+    private String resourceField;
+    private String resourceValue;
     public ResourceNotFoundException(String resourceName, String resourceField, int resourceValue) {
         super(String.format("%s not found with %s : %d ", resourceName,resourceField,Long.valueOf(resourceValue)));
+        this.resourceName=resourceName;
+        this.resourceField=resourceField;
+        this.resourceValue=Integer.toString(resourceValue);
     }
 }
