aa503898b4d891eee873359a229b566346b785c9
KubraErmeydan <73437863+KubraErmeydan@users.noreply.github.com>
2023-05-20 23:35:40 +0300

    Hata kısmı düzenlendi.

--- src/main/java/com/eminetekcan/BlogApp/exception/ResourceNotFoundException.java
+++ src/main/java/com/eminetekcan/BlogApp/exception/ResourceNotFoundException.java
@@ -6,7 +6,7 @@ public class ResourceNotFoundException extends RuntimeException{
     private String resourceField;
     private String resourceValue;
     public ResourceNotFoundException(String resourceName, String resourceField, int resourceValue) {
-        super(String.format("%s not found with %s : %d ", resourceName,resourceField,Long.valueOf(resourceValue)));
+        super(String.format("%s not found object with %s : %d ", resourceName,resourceField,Long.valueOf(resourceValue)));
         this.resourceName=resourceName;
         this.resourceField=resourceField;
         this.resourceValue=Integer.toString(resourceValue);
